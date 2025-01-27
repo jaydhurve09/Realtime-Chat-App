715b1c752c9420764823428aa12895ee6de54752
jaydhurve09 <jaydhurve09@gmail.com>
2025-01-27 23:16:17 +0530

    Message route and controller created

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import authRoutes from './routes/auth.route.js'
+import messageRoutes from './routes/message.route.js'
 import dotenv from 'dotenv'
 import { connectDB } from './lib/db.js';
+import cookieParser from "cookie-parser"
 
 
 dotenv.config();
@@ -10,8 +12,10 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/message', messageRoutes);
 
 app.get('/', (req, res)=>{
     res.sendStatus(200);
