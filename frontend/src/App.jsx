8c205da7d575e3b7297754388711e67d691ff0c0
jaydhurve09 <jaydhurve09@gmail.com>
2025-01-29 21:20:36 +0530

    daisyui plugin installed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,10 @@ const App = () => {
   return (
     <div>
       <h2 className='text-3xl text-red-500 font-bold underline'>Hello World from Another World</h2>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
     </div>
   )
 }
